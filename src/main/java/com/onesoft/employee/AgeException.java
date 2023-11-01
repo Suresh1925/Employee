55b3ff5b38c6a4968cd44449a6020bb0b392cbf3
Suresh1925 <Jerry25suresh@gmail.com>
2023-11-01 15:04:27 +0530

    Age Exception corrected

--- src/main/java/com/onesoft/employee/AgeException.java
+++ src/main/java/com/onesoft/employee/AgeException.java
@@ -3,8 +3,8 @@ package com.onesoft.employee;
 import java.rmi.AccessException;
 
 public class AgeException extends AccessException {
-		public AgeException(String msg) {
-			super(msg);
+		public AgeException(String message) {
+			super(message);
 		}
 	
 
